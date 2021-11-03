981f7419ef252edae155b7c0b28124af2467d9db
Mihail Feraru <mferaru@bitdefender.com>
2021-11-03 19:09:00 +0200

    Fix app

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ import threading
 from datetime import datetime
 import flask
 from pprint import pprint
+from time import sleep
 
 # Import all the apps
-# import smarttv
 import windwow
+from windwow.api import default_api as windwow_default_api
 import smarttv
+from smarttv.api import default_api as smarttv_default_api
 import smartkettle
+from smartkettle.api import default_api as smartkettle_default_api
+import flowerpower
+from flowerpower.api import default_api as flowerpower_default_api
+ 
+# Hostnames (inside docker) are the same as client libs names
+def get_api_instance(lib, api):
+    print(f"Instanciate API client for {lib.__name__}")
+    configuration = lib.Configuration(host = f"http://{lib.__name__}:9080")
+    api_client = lib.ApiClient(configuration)
+    return api.DefaultApi(api_client)
 
-# Hostnames for running applications (taken from docker)
-"""
-      - "flowerpower"
-      - "smarteeth"
-      - "windwow"
-      - "smarttv"
-      - "smartkettle"
-"""
-
-from os import system
-system("ping flowerpower")
-exit()
+windwow_api_instance = get_api_instance(windwow, windwow_default_api)
+smartkettle_api_instance = get_api_instance(smartkettle, smartkettle_default_api)
 
 class general_environment:
     now = 0
@@ -35,7 +37,6 @@ class general_environment:
         self.set_time()
 
     def set_time(self):
-        threading.Timer(1.0, self.set_time).start()
         self.now = datetime.now()
 
     def print_env_values(self):
@@ -43,32 +44,33 @@ class general_environment:
 
 env = general_environment();
 
-# try:
-#     insert_user_username_varsta_post_response = smarttv.api_instance.insert_user_username_varsta_post("ionel", 19)
-#     pprint(insert_user_username_varsta_post_response)
-# except smarttv.smarttv_object.ApiException as e:
-#     print("Exception when calling DefaultApi->add_channel_username_canal_post: %s\n" % e)
+print("Wait 5 seconds before starting (so we are sure that all apps started)")
+sleep(5)
 
 #### 
 ## Test Case 1 - Get luminosity from WindWow and stir SmartKettle with the value of the luminosity
+print ("Test case 1")
 
 ## Prerequisite environment values
 
 # Set default environment luminosity value
-set_luminosity = windwow.api_instance.settings_setting_name_setting_value_post("luminosity",env.luminosity)
+set_luminosity = windwow_api_instance.settings_setting_name_setting_value_post("luminosity",env.luminosity)
+print(f"settings_setting_name_setting_value_post: {set_luminosity}")
 
 ##
 
 ## Test Scenario
 
 # Get luminosity
-get_luminosity = windwow.api_instance.settings_setting_name_get("luminosity")
+get_luminosity = windwow_api_instance.settings_setting_name_get("luminosity")
+print(f"settings_setting_name_get: {get_luminosity}")
 
 # Parse luminosity and save it as a general environment value
 env.luminosity = int(get_luminosity[14:16])
 
 # Set the stir of SmartKettle
-stir_liquid_rpm_get_response = smartkettle.api_instance.stir_liquid_rpm_get(env.luminosity)
+stir_liquid_rpm_get_response = smartkettle_api_instance.stir_liquid_rpm_get(env.luminosity)
+print(f"stir_liquid_rpm_get_response: {stir_liquid_rpm_get_response}")
 
 ####
 
---
+++ docker_publish.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t thelittlewho/hub_app . && docker push thelittlewho/hub_app:latest
