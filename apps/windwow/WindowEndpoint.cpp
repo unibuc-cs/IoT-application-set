03792e380015b5107a983d4f0f588972bfa8a4aa
JustBeYou <mihailferaru2000@gmail.com>
2022-01-13 10:18:38 +0200

    comment out artificial bugs

--- apps/windwow/WindowEndpoint.cpp
+++ apps/windwow/WindowEndpoint.cpp
@@ -347,10 +347,10 @@ private:
         else {
             // START FAKE BUG
             // No response, long wait
-            std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+            //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
             // END FAKE BUG
 
-            //response.send(Http::Code::Not_Found, settingName + " was not found");
+            response.send(Http::Code::Not_Found, settingName + " was not found");
         }
     }
       // Create the lock which prevents concurrent editing of the same variable
