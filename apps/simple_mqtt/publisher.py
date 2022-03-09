e840f2900697225dc2dfa549e5fd9a62be8ef618
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-03-09 15:21:14 +0200

    Add simple MQTT network with 1 publisher, 1 subscriber and 1 broker

---
+++ apps/simple_mqtt/publisher.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010-2013 Roger Light <roger@atchoo.org>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Distribution License v1.0
+# which accompanies this distribution.
+#
+# The Eclipse Distribution License is available at
+#   http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# Contributors:
+#    Roger Light - initial implementation
+# Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
+# All rights reserved.
+
+# This shows a simple example of an MQTT subscriber.
+
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(mqttc, obj, flags, rc):
+    print("[CONNECT] " + str(rc))
+
+def on_message(mqttc, obj, msg):
+    print("[MESSAGE] " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("[PUBLISH] " + str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("[SUBSCRIBE] " + str(mid) + " " + str(granted_qos))
+
+def on_log(mqttc, obj, level, string):
+    print("[LOG] " + string)
+
+
+# If you want to use a specific client id, use
+# mqttc = mqtt.Client("client-id")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+# Uncomment to enable debug messages
+# mqttc.on_log = on_log
+mqttc.connect("172.26.0.2", 1883, 60)
+for i in range(5):
+	time.sleep(2)
+	mqttc.publish("sensor/temperature", i)
+
+mqttc.loop_stop()
