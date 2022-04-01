550b0bebb17cffb695e608eaf058057798254015
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-04-01 16:47:08 +0100

    Extend smartkettle to work on RPi

--- apps/smartkettle/build.sh
+++ apps/smartkettle/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-g++ ../smart_kettle.cpp -o main -lpistache -lmosquitto -lcrypto -lssl -lpthread  -lboost_program_options -std=c++17
+g++ ../smart_kettle.cpp -o main -I ../../../pistache/include -L ../../../pistache/build/src -lpistache --lboost_program_options -lmosquitto -lcrypto -lssl -lpthread  -std=c++17
+
