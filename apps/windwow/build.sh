0052bb841124616db9b1b7c43125eab8c0754866
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-04-05 16:32:10 +0100

    Extend smarttv and windwow to work on RPi

--- apps/windwow/build.sh
+++ apps/windwow/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ -std=c++17 ../Server.cpp -o server -lpistache -lcrypto -lssl -lpthread -lboost_program_options && \
+g++ -std=c++17 ../Server.cpp -o server -I ../../../pistache/include -L ../../../pistache/build/src -lpistache -lcrypto -lssl -lpthread -lboost_program_options && \
     chmod 755 ./server
