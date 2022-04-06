57834ee4c0887ef6702e1595ce4d3f3ce124834d
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-04-06 16:15:59 +0300

    Small fix for flowerpower and smartkettle builds

--- apps/smartkettle/build.sh
+++ apps/smartkettle/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ ../smart_kettle.cpp -o main -I ../../../pistache/include -L ../../../pistache/build/src -lpistache --lboost_program_options -lmosquitto -lcrypto -lssl -lpthread  -std=c++17
+g++ ../smart_kettle.cpp -o main -I ../../../pistache/include -L ../../../pistache/build/src -lpistache -lboost_program_options -lmosquitto -lcrypto -lssl -lpthread  -std=c++17
 
