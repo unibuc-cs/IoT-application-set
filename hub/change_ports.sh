30eecec793a58c8bdb3443d6726a1561205d8398
Mihail Feraru <mihailferaru2000@gmail.com>
2022-04-11 14:59:29 +0000

    Add serasmart to change ports script

--- hub/change_ports.sh
+++ hub/change_ports.sh
@@ -5,4 +5,6 @@ sed -i 's/smartkettle:[0-9][0-9][0-9][0-9]/localhost:9081/g' clients/mergedclien
 sed -i 's/flowerpower:[0-9][0-9][0-9][0-9]/localhost:9082/g' clients/mergedclients/mergedclients/all.py
 sed -i 's/smarttv:[0-9][0-9][0-9][0-9]/localhost:9083/g' clients/mergedclients/mergedclients/all.py
 sed -i 's/windwow:[0-9][0-9][0-9][0-9]/localhost:9084/g' clients/mergedclients/mergedclients/all.py
+sed -i 's/serasmart:[0-9][0-9][0-9][0-9]/localhost:9085/g' clients/mergedclients/mergedclients/all.py
+
 
