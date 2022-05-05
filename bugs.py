0d695cf99b3300ddb27b7f42a0cec9f60978dad2
Mihail Feraru <mferaru@bitdefender.com>
2022-05-05 16:32:46 +0300

    update bugs script

--- bugs.py
+++ bugs.py
@@ -1,7 +1,15 @@
+"""
+Script to enable and disable bugs.
+Usage: python3 bugs.py <"enable"|"disable"> ["step"]
+    enable/disable: apply or revert git patches
+    step: interactive mode
+"""
+
 from sys import argv
 from os import system, listdir
 
 enable = True if len(argv) > 1 and argv[1] == "enable" else False
+step = True if len(argv) > 2 and argv[2] == "step" else False
 
 for f in listdir("./bug_unpatches"):
     if 'patch' not in f: continue
@@ -10,3 +18,6 @@ for f in listdir("./bug_unpatches"):
     cmd = f"git apply {'-R' if not enable else ''} ./bug_unpatches/{f}"
     print(cmd)
     system(cmd)
+
+    if step:
+        input("Press enter to continue...")
